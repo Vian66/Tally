fb49e39f8c3d9dd37f322d05560563f3f228a61a
TallyTools <vicky926better801637@gmail.com>
2022-01-18 17:29:14 +0800

    tree

--- wwwroot/src/components/index.vue
+++ wwwroot/src/components/index.vue
@@ -1,23 +1,112 @@
 <template>
   <el-container>
       <el-header style="height: 30px;">Tally && TallyParent</el-header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <ul class="projectInfo">
+          <li>專案:</li>
+          <li><el-input
+              size="small"
+             v-model="input" placeholder="請選擇專案"></el-input>
+          </li>
+          <li>Tally</li>
+          <li><el-switch
+                v-model="value"
+                active-color="#13ce66"
+                inactive-color="#0d6efd">
+              </el-switch>
+          </li>
+          <li>TallyParent</li>
+          <div class="right">
+            <li>First node:</li>
+            <li><el-button size="small" type="primary">Add</el-button></li>
+           <li>不顯示</li>
+            <li>
+              <el-switch v-model="value1"></el-switch>
+            </li>
+            <li>顯示刪除項目</li>
+            <li><el-upload
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-change="handleChange">
+            <el-button size="small" type="primary">匯入</el-button>
+            <div slot="tip" class="el-upload__tip"></div>
+            
+          </el-upload></li>
+          <li><el-button size="small" type="primary">匯出</el-button></li>
+          </div>
+          <li><el-button size="small" class="btn" type="primary">Save</el-button></li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Tree -->
+     <el-tree
+      :data="data"
+      node-key="id"
+      default-expand-all
+      @node-drag-start="handleDragStart"
+      @node-drag-enter="handleDragEnter"
+      @node-drag-leave="handleDragLeave"
+      @node-drag-over="handleDragOver"
+      @node-drag-end="handleDragEnd"
+      @node-drop="handleDrop"
+      draggable
+      :allow-drop="allowDrop"
+      :allow-drag="allowDrag">
+      
+                    
+               
+    </el-tree>
+      
   </el-container>
 </template>
-<style>
-</style>
-<script leng="ts">
+<script>
   module.exports = {
     props: [
       "sys_path"
     ],
     data() {
       return {
-        
+        input: '',
+        value: true,
+        value1: true,
+         data: [],
+
       }
     },
     methods: {
-     
-      
+        handleChange(file, fileList) {
+        this.fileList = fileList.slice(-3);
+      },
+              handleDragStart(node, ev) {
+        console.log('drag start', node);
+      },
+      handleDragEnter(draggingNode, dropNode, ev) {
+        console.log('tree drag enter: ', dropNode.label);
+      },
+      handleDragLeave(draggingNode, dropNode, ev) {
+        console.log('tree drag leave: ', dropNode.label);
+      },
+      handleDragOver(draggingNode, dropNode, ev) {
+        console.log('tree drag over: ', dropNode.label);
+      },
+      handleDragEnd(draggingNode, dropNode, dropType, ev) {
+        console.log('tree drag end: ', dropNode && dropNode.label, dropType);
+      },
+      handleDrop(draggingNode, dropNode, dropType, ev) {
+        console.log('tree drop: ', dropNode.label, dropType);
+      },
+      allowDrop(draggingNode, dropNode, type) {
+        if (dropNode.data.label === '二级 3-1') {
+          return type !== 'inner';
+        } else {
+          return true;
+        }
+      },
+      allowDrag(draggingNode) {
+        return draggingNode.data.label.indexOf('三级 3-2-2') === -1;
+      }
     }
   }
 </script>
